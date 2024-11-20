a4aa37b15ce8f3f73a313dd48c50079fd679f42e
Keenan Powell <kmodo1234@gmail.com>
2024-11-20 17:28:00 -0500

    adding finished read_data.py and removing more pycache files

--- read_data.py
+++ read_data.py
@@ -4,76 +4,71 @@ import os
 import numpy as np
 import seaborn as sns
 
-constraint = "GreenTime"
-env = "JN"
-results_folder = "/cmlscratch/anirudhs/ctraffic/results"
-results_prefixes = [
-    f"ippo_{env}_{constraint}",
-    f"mappo_{env}_{constraint}",
-    f"mappolce_{env}_{constraint}",
-    f"qtran_{env}_{constraint}",
-]
+results_folder = "./results_copy/results_copy"
+results_combined = {}
+for f in os.listdir(results_folder):
+    algo, env, constraint, *_ = f.split("_")
+    if algo not in {'mappo', 'ippo', 'mappolce', 'qtran'}: continue
+    if not env in results_combined: results_combined[env] = {}
+    if not constraint in results_combined[env]: results_combined[env][constraint] = {}
+    if not algo in results_combined[env][constraint]: results_combined[env][constraint][algo] = []
+    with open(f"{results_folder}/{f}", "rb") as file:
+            results_combined[env][constraint][algo].append(pkl.load(file))
 
+constraint_to_idx = {'None': 0, 'GreenTime': 1, 'PhaseSkip': 2, 'GreenSkip': 3}
+algo_to_idx = {'mappolce': 0, 'mappo': 1, 'qtran': 2, 'ippo': 3}
 
-def plot_all_runs(prefix_list, key):
-    colors = ["red", "blue", "green", "orange", "purple"]
-    plt.figure()
+def plot_all_runs(results_combined, key, title_key):
     sns.set_style("whitegrid")
+    fig, axs = plt.subplots(len(results_combined), len(results_combined['HZ']))
+    fig.set_figwidth(25)
+    fig.set_figheight(15)
+    for j, env in enumerate(results_combined):
+        for constraint in results_combined[env]:
+            colors = ["black", "red", "blue", "green", "orange", "purple"]
+            for algo in results_combined[env][constraint]:
+                results_list = results_combined[env][constraint][algo] 
+                # only include the longest runs
+                results_list = [r for r in results_list if len(r) == len(max(results_list, key=len))]
 
-    for i, prefix in enumerate(prefix_list):
-        results_files = [f for f in os.listdir(results_folder) if f.startswith(prefix)]
-        assert len(results_files) > 0, "No files found with prefix " + prefix
+                # Prepare data for plotting
+                timesteps = [d["Time Step"]/100000 for d in results_list[0] if key in d]
+                rewards = np.array([[d[key] for d in r if key in d] for r in results_list])
 
-        results_list = []
-        for f in results_files:
-            with open(results_folder + "/" + f, "rb") as file:
-                results_list.append(pkl.load(file))
+                # Smoothing
+                smooth_window = 20
+                ones_smooth = np.ones(smooth_window)
+                smoothed_rewards = []
+                for r in rewards:
+                    smoothed_r = np.convolve(r, ones_smooth, "same") / np.convolve(
+                        np.ones(len(r)), ones_smooth, "same"
+                    )
+                    smoothed_rewards.append(smoothed_r)
+                smoothed_rewards = np.array(smoothed_rewards)
 
-        # Filter to the longest runs
-        max_length = max(
-            len([i[key] for i in result if key in i]) for result in results_list
-        )
-        longest_results_list = [
-            result
-            for result in results_list
-            if len([i[key] for i in result if key in i]) == max_length
-        ]
-        results_list = longest_results_list[: min(3, len(longest_results_list))]
+                # Calculate averages and bounds
+                avg_reward = np.mean(smoothed_rewards, axis=0)
+                min_reward = np.min(smoothed_rewards, axis=0)
+                max_reward = np.max(smoothed_rewards, axis=0)
 
-        # Prepare data for plotting
-        timesteps = [d["Time Step"] for d in results_list[0] if key in d]
-        rewards = np.array([[d[key] for d in r if key in d] for r in results_list])
+                # Plot the average reward and shaded region for min-max
+                axs[j][constraint_to_idx[constraint]].plot(timesteps, avg_reward, color=colors[algo_to_idx[algo]], label=f"{algo}")
+                axs[j][constraint_to_idx[constraint]].fill_between(timesteps, min_reward, max_reward, alpha=0.2, color=colors[algo_to_idx[algo]])
 
-        # Smoothing
-        smooth_window = 20
-        ones_smooth = np.ones(smooth_window)
-        smoothed_rewards = []
-        for r in rewards:
-            smoothed_r = np.convolve(r, ones_smooth, "same") / np.convolve(
-                np.ones(len(r)), ones_smooth, "same"
-            )
-            smoothed_rewards.append(smoothed_r)
-        smoothed_rewards = np.array(smoothed_rewards)
-
-        # Calculate averages and bounds
-        avg_reward = np.mean(smoothed_rewards, axis=0)
-        min_reward = np.min(smoothed_rewards, axis=0)
-        max_reward = np.max(smoothed_rewards, axis=0)
-
-        # Plot the average reward and shaded region for min-max
-        plt.plot(timesteps, avg_reward, color=colors[i], label=f"{prefix}_average")
-        plt.fill_between(timesteps, min_reward, max_reward, alpha=0.2, color=colors[i])
-
-    plt.legend()
-    plt.xlabel("Time Step")
-    plt.ylabel(key)
-    plt.title(f"Reward Comparison for {key}")
+            #axs[j][constraint_to_idx[constraint]].legend()
+            #axs[j][constraint_to_idx[constraint]].set_xlabel("Time Step (100,000s)")
+            #axs[j][constraint_to_idx[constraint]].set_ylabel(title_key)
+            axs[j][constraint_to_idx[constraint]].set_title(f"{constraint} constraint in {env}" if constraint != 'None' else
+                                f"No constraint in {env}", fontsize=20)
+    plt.suptitle(f"Averaged {title_key} vs. Time Step (100,000s)", fontsize=30)
+    plt.tight_layout(pad=1)
+    fig.subplots_adjust(top=0.90, bottom=0.075)
+    handles, labels = axs[0][0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', prop={'size': 25}, ncols=4) 
     plt.savefig(
-        f"{key} {constraint} {env}.png"
-    )  # Save the combined plot after the loop
-    plt.show()  # Optionally show the plot if running interactively
+        f"./{title_key}.png"
+    )  # Save the combined plot after the loop  
 
-
-plot_all_runs(results_prefixes, "Test Reward")
-plot_all_runs(results_prefixes, "test_throughput")
-plot_all_runs(results_prefixes, "test_average_delay")
+plot_all_runs(results_combined, "Test Reward", "Test Reward")
+plot_all_runs(results_combined, "test_throughput", "Test Throughput")
+plot_all_runs(results_combined, "test_average_delay", "Test Average Delay")
